dd806611312cf5764fe83a552f4b59f00e3e57d4
cla114 <clayton.rossiter@gmail.com>
2020-06-16 18:09:55 +0100

    add sound to resource collision

--- js/Resource.js
+++ js/Resource.js
@@ -1,6 +1,11 @@
 export default class Resource extends Phaser.Physics.Matter.Sprite {
     static preload (scene){
+        // non-terrain resources
         scene.load.atlas('resources','assets/images/resources.png','assets/images/resources_atlas.json');
+        // sound effects
+        scene.load.audio('tree','assets/audio/tree.mp3');
+        scene.load.audio('rock','assets/audio/rock.mp3');
+        scene.load.audio('bush','assets/audio/bush.mp3');
     }
 
     constructor(data){
@@ -8,11 +13,18 @@ export default class Resource extends Phaser.Physics.Matter.Sprite {
         super(scene.matter.world, resource.x, resource.y, 'resources', resource.type);
         this.scene.add.existing(this);
 
+        // load sounds
+        this.name = resource.type;
+        this.sound = this.scene.sound.add(this.name);
+
+        // default health
         this.health = 5;
 
+        // load resources
         let yOrigin = resource.properties.find(p=>p.name == 'yOrigin').value;
         this.y = this.y + this.height * (yOrigin - 0.5);
 
+        // resource colliders and sensors
         const{Body,Bodies} = Phaser.Physics.Matter.Matter;
         var circleCollider = Bodies.circle(this.x, this.y,12,{isSensor:false,label:'collider'});
         this.setExistingBody(circleCollider);
